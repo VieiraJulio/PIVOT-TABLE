543f1bb985596dd162a66066769e9e4c46adfb41
Julio Vieira <juliostefano1327@gmail.com>
2024-08-11 18:48:54 -0300

    Pivot table funcional, porém manual. 1.01

--- Pivot Table.sql
+++ Pivot Table.sql
@@ -8,3 +8,37 @@ SELECT
 FROM
 	DimProduct
 GROUP BY BrandName
+
+
+SELECT 
+	*
+FROM
+	(
+	SELECT
+		ProductKey,
+		BrandName
+	
+	FROM
+		DimProduct
+	) AS Dados 
+PIVOT(
+	COUNT(ProductKey)
+	FOR BrandName
+-- TESTANDO DE MODO MANUAL A PRIMEIRO MODO : SELECT DISTINCT ',[' + TRIM(BrandName) + ']' FROM DimProduct
+		IN ( 
+	[Northwind Traders]
+	,[Contoso]
+	,[Tailspin Toys]
+	,[Adventure Works]
+	,[Southridge Video]
+	,[Wide World Importers]
+	,[The Phone Company]
+	,[Fabrikam]
+	,[Litware]
+	,[A. Datum]
+	,[Proseware]
+	)
+
+) AS PivotTable
+
+
