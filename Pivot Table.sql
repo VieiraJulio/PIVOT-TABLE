3cbe0ee4b39e06ba045181674aaf8f15d0bf556b
Julio Vieira <juliostefano1327@gmail.com>
2024-08-12 18:28:12 -0300

    Grupo de linha adicionado - PivotTable

--- Pivot Table.sql
+++ Pivot Table.sql
@@ -1,5 +1,5 @@
 -- PIVOT TABLE 
-
+/*
 USE ContosoRetailDW
 
 SELECT
@@ -39,6 +39,57 @@ PIVOT(
 	,[Proseware]
 	)
 
+) AS PivotTable
+
+
+*/
+
+-- GRUPOS DE LINHA 
+
+USE ContosoRetailDW
+
+SELECT 
+	DepartmentName,
+	COUNT(*) AS 'Total de funcionários'
+FROM
+	DimEmployee
+GROUP BY DepartmentName
+
+
+
+SELECT 
+	*
+FROM
+	
+	(SELECT 
+		EmployeeKey,
+		DepartmentName,
+		YEAR(HireDate) AS 'Ano'
+	FROM
+		DimEmployee) AS Dados
+PIVOT(
+	COUNT(EmployeeKey)
+	FOR DepartmentName
+-- SELECT DISTINCT ',[' + TRIM(DepartmentName) + ']' FROM DimEmployee
+	IN(
+		[Document Control]
+		,[Engineering]
+		,[Executive]
+		,[Facilities and Maintenance]
+		,[Finance]
+		,['Human Resources Contral]
+		,[Human Resources]
+		,[Information Services]
+		,[Marketing]
+		,[Production Control]
+		,[Production]
+		,[Purchasing]
+		,[Quality Assurance]
+		,[Research and Development]
+		,[Sales]
+		,[Shipping and Receiving]
+		,[Tool Design]
+		)
 ) AS PivotTable
 
 
